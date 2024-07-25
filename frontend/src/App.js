8ceb31f855fc8e404bcb7c0570b9919aee7aafce
Marc Garibal <garibal.marc@gmail.com>
2024-07-25 11:12:11 +0200

    Configuration ReactAdmin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import restProvider from 'ra-data-simple-rest';
+import authProvider from './authProvider';
+import { UtilisateurList, UtilisateurEdit, UtilisateurCreate } from './resources/utilisateurs';
+import { ContratList, ContratEdit, ContratCreate } from './resources/contrats';
+import { QuittanceList, QuittanceEdit, QuittanceCreate } from './resources/quittances';
+import { TransactionList, TransactionEdit, TransactionCreate } from './resources/transactions';
+import { MoyenDePaiementList, MoyenDePaiementEdit, MoyenDePaiementCreate } from './resources/moyenDePaiements';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = restProvider('http://localhost:8080/api');
+
+const App = () => (
+    <Admin dataProvider={dataProvider} authProvider={authProvider} title="QuittanceSwift">
+      <Resource name="utilisateurs" list={UtilisateurList} edit={UtilisateurEdit} create={UtilisateurCreate} />
+      <Resource name="contrats" list={ContratList} edit={ContratEdit} create={ContratCreate} />
+      <Resource name="quittances" list={QuittanceList} edit={QuittanceEdit} create={QuittanceCreate} />
+      <Resource name="transactions" list={TransactionList} edit={TransactionEdit} create={TransactionCreate} />
+      <Resource name="moyenDePaiements" list={MoyenDePaiementList} edit={MoyenDePaiementEdit} create={MoyenDePaiementCreate} />
+    </Admin>
+);
 
 export default App;
